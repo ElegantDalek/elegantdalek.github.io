46a20e6f70415f77b1aa1ae50e323e1598508f63
Theodore Li <karatedkid@gmail.com>
2016-06-13 18:35:24 -0500

    Add cover photo

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var main = function(){
 	$('.btn-menu').click(function(){
-        $('.menu').animate({
-            left: '220px'
-    }, 200);
+    	$('.menu').animate({
+    		left: "0px"
+    	}, 200);
+    	$(this).addClass('menu-close');
+    	$(this).removeClass('btn-menu');
+    });
+
+	$('.menu-close').click(function(){
+		$('.menu').animate({
+			left: "-285px"
+		}, 200);
+		$(this).addClass('btn-menu');
+		$(this).removeClass('menu-close');
+	});
 
 };
+
 $(document).ready(main);
